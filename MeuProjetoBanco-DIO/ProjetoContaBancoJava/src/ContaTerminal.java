a2f8aa4739140ea845fd559f5e5dc713bf5eaa63
EduardoVianaS <viana.eduardo889@gmail.com>
2024-05-06 13:58:50 -0300

    Projeto banco

--- MeuProjetoBanco-DIO/ProjetoContaBancoJava/src/ContaTerminal.java
+++ MeuProjetoBanco-DIO/ProjetoContaBancoJava/src/ContaTerminal.java
@@ -1,15 +1,26 @@
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+import java.util.Scanner;
+
 public class ContaTerminal {
     public static void main(String[] args) {
-        //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
-        // to see how IntelliJ IDEA suggests fixing it.
-        System.out.printf("Hello and welcome!");
+        Scanner sc = new Scanner(System.in);
+        int Numero;
+        String Agencia;
+        String nomeCliente;
+        double saldo;
 
-        for (int i = 1; i <= 5; i++) {
-            //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
-            // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
-            System.out.println("i = " + i);
+        System.out.println("-----CADASTRO DE CONTA-----");
+        System.out.println("Insira o numero da conta: ");
+        Numero = sc.nextInt();
+        sc.nextLine();
+        System.out.println("Insira a agencia: ");
+        Agencia = sc.nextLine();
+        System.out.println("Insira o nome do cliente: ");
+        nomeCliente = sc.nextLine();
+        System.out.println("Insira o saldo: ");
+        saldo = sc.nextDouble();
+
+        System.out.println("Ola! " + nomeCliente + ", obrigado por criar uma conta em nosso banco, sua agência é " + Agencia + ", número da conta " + Numero + ", e seu saldo de " + saldo + " já esta disponivel para saque!");
+
+
         }
     }
-}
