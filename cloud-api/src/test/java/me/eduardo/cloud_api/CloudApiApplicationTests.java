535e122e9b2c03a8e3b07f402737d88939face51
Eduardo Viana da Silva (AWNER) <eduardo.vianadasilva@awner.net>
2024-07-02 14:20:50 -0300

    RESTFul API Cloud deploy

---
+++ cloud-api/src/test/java/me/eduardo/cloud_api/CloudApiApplicationTests.java
@@ -0,0 +1,13 @@
+package me.eduardo.cloud_api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CloudApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
